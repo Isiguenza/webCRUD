0197c05e4e665b4081c3af1cb83ef5d190f6d3c5
Iñaki Sigüenza <isiguenzan@gmail.com>
2021-10-22 12:58:11 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFw3Rgt7d1VX6qsBedwXCD8VJ-klhzIS4",
+  authDomain: "isn-crud.firebaseapp.com",
+  projectId: "isn-crud",
+  storageBucket: "isn-crud.appspot.com",
+  messagingSenderId: "357849786925",
+  appId: "1:357849786925:web:f2a07f52d435198d53e6a4"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
